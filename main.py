cb1a36d8f4ef4196b8d6fb062ef69b8b3430068b
anthonychiumb <chiua1688@gmail.com>
2026-08-13 11:35:05 -0700

    B: add env to gitignore, and created response request api call

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+from openai.types.chat import ChatCompletionMessageParam
+
+load_dotenv()
+api_key = os.environ.get("OPENROUTER_API_KEY")
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
+
 def main():
-    print("Hello from ai-agent!")
+    messages: list[ChatCompletionMessageParam] = [
+        {
+            "role": "user",
+            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        }
+    ]
+
+    response = client.chat.completions.create(
+        model="openrouter/free", messages=messages
+    )
+    print(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
